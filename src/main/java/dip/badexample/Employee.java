09dc3a1b12b088b86eda1b9fc6ba534a9a04b016
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-04-11 21:02:14 +0200

    Inicio práctica DIP.
    Se agrega el código propuesto para la clase 30 de la sección 10. Este código no cumple el principio de DIP.

---
+++ src/main/java/dip/badexample/Employee.java
@@ -0,0 +1,14 @@
+package dip.badexample;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Employee {
+
+    private String fullName;
+
+    private Integer salary;
+
+}
---
+++ src/main/java/dip/badexample/EmployeePersistence.java
@@ -0,0 +1,11 @@
+package dip.badexample;
+
+import java.util.List;
+
+public interface EmployeePersistence {
+
+    List<Employee> findAll();
+
+    void save(Employee employee);
+
+}
